3878dfc42f5bed497f0a17a65c6c15c7bbd48c50
Uyi Obazee <utejoe.ju@gmail.com>
2023-11-06 15:29:56 +0100

    committing changes in my project

--- my-portfolio/js/script.js
+++ my-portfolio/js/script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*################# typed js ######################*/
     const typed = new Typed('.multiple-text',{
-        strings: ['Software Engineer', 'Civil Engineer', 'Trader', 'Farmer', 'Building Designer (using Autocad and Revit)'],
+        strings: ['Software Engineer', 'Civil Engineer', 'Building Designer (using Autocad and Revit)'],
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 1000,
